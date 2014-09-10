de0b27ed7d7ee63fded84db4b086663b72ffec1b
Slim <luccy559slim@gmail.com>
2014-09-10 14:32:13 +0100

    Update2

--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@ var Actions = {
 			if(Actions.validate()) {
 				Actions.createNewListItem();
 				prevInputs.push(listItem);
+				$('#alert_area').fadeOut('slow');
 			} else {
 				$('#alert_area').html('Your Entry must not be empty, must not be more than 20 characters, and must not already exist in the List!');
 			}
